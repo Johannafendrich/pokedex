36c15298fc86591947d7ad4ef63687e274268e34
Johannafendrich <fendrichjohanna@gmail.com>
2020-03-04 16:25:02 +0100

    fix filterfunctions

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,7 @@
 import './search.scss';
 import { createElement } from '../lib/dom';
 
-
 export function createSearchInput(searchValue) {
-
-export function search(searchValue) {
-
   const element = createElement('input', {
     className: 'search',
     type: 'search',
