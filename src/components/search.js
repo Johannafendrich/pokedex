ed2ea80575b4999db7d167e05f89bbd8fd82423a
Johannafendrich <fendrichjohanna@gmail.com>
2020-03-04 15:19:34 +0100

    Add searchinput and create a storage

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,12 @@
 import './search.scss';
 import { createElement } from '../lib/dom';
 
-export function search() {
+export function createSearchInput(searchValue) {
   const element = createElement('input', {
     className: 'search',
     type: 'search',
-    placeholder: 'what are you looking for?'
+    placeholder: 'what are you looking for?',
+    value: searchValue
   });
   return element;
 }
