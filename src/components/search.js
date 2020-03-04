1b878b980bf3afdca7c3030c78e75166bd3ced70
Johannafendrich <fendrichjohanna@gmail.com>
2020-03-04 13:53:38 +0100

    Put DS-store to git ignore

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,12 @@
 import './search.scss';
 import { createElement } from '../lib/dom';
 
-export function search() {
+export function search(searchValue) {
   const element = createElement('input', {
     className: 'search',
     type: 'search',
-    placeholder: 'what are you looking for?'
+    placeholder: 'what are you looking for?',
+    value: searchValue
   });
   return element;
 }
