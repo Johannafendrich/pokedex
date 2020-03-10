99355eb8a84b1bf6183379936ebc17fa12f326e2
Johannafendrich <fendrichjohanna@gmail.com>
2020-03-10 10:00:01 +0100

    Add viewport

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,10 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Pokedex'
+      title: 'Pokedex',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1.0, shrink-to-fit=no'
+      }
     })
   ],
   devServer: {
